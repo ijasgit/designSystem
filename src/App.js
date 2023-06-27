dca2401afb1602905f97715a7491150696019042
partha2711 <parthasarathi@archimedis.io>
2023-06-27 16:45:07 +0530

    commit fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import "./App.css";
 import SignInPage from "./Components/Sign-In page/SignInPage";
 // import Home from "./Components/NavBar/Home";
 // import NavBar from "./Components/NavBar/NavBar";
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import SignUp from "./Components/Sign-In page/SignUp";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignInPage/>}> </Route>
+      <Route path="/signup" element={<SignUp/>}> </Route> 
+
+    </Routes>  
+    </BrowserRouter>
       {/* <Home/> */}
-      <SignInPage/>
+      {/* <SignInPage/> */}
     </>
   );
 }
