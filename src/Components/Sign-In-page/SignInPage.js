e26eb869cbc508d02b090a0b97610c4074fc8d43
ijasgit <you@example.com>
2023-07-24 15:02:44 +0530

    .

--- src/Components/Sign-In-page/SignInPage.js
+++ src/Components/Sign-In-page/SignInPage.js
@@ -171,9 +171,7 @@ const[passwordVisible,setpasswordvisible]=useState(true)
           <span>{usercond?<span style={{color:"red",paddingLeft:"10px",fontSize:"12px"}}>*Enter E-mail</span>:null}</span>  
           <span>{invalidEmail?<span style={{color:"red",paddingLeft:"10px",fontSize:"12px"}}>*Invalid E-mail</span>:null}</span>  
 
-            
-    
-            <TextField
+              <TextField
               margin="normal"
               required
               fullWidth
@@ -194,14 +192,17 @@ const[passwordVisible,setpasswordvisible]=useState(true)
                
               }}
             />
+             <div className="aiOutlet-icon" >
+              {/* <AiOutlineEye style={{ fontSize: "22px" }}></AiOutlineEye> */}
+           {passwordVisible?   <VisibilityIcon onClick={()=>setpasswordvisible(false)} />: <VisibilityOffIcon onClick={()=>setpasswordvisible(true)}/>}
+            </div>
+    
+          
              <span>{passwordcond?<span style={{color:"red",paddingLeft:"10px",fontSize:"12px"}}>*Enter password</span>:null}</span>  
              <span>{invalidpassword?<span style={{color:"red",paddingLeft:"10px",fontSize:"12px"}}>*Incorrect password</span>:null}</span>  
 
 
-            <div className="aiOutlet-icon">
-              {/* <AiOutlineEye style={{ fontSize: "22px" }}></AiOutlineEye> */}
-           {passwordVisible?   <VisibilityIcon onClick={()=>setpasswordvisible(false)} />: <VisibilityOffIcon onClick={()=>setpasswordvisible(true)}/>}
-            </div>
+           
 
             <Button
               fullWidth
